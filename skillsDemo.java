ebbde8231d55e798625500c3d66164322695f736
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-02-04 10:08:25 -0800

    Updating java file with method and creating tester file

--- skillsDemo.java
+++ skillsDemo.java
@@ -1,2 +1,5 @@
 public class skillsDemo{
+    public static int multiply(int x, int y){
+        return x * y;
+    }
 }
---
+++ skillsDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillsDemoTest {
+    @Test
+    public void multiplication() {
+        assertEquals(3, skillsDemo.multiply(1,2));
+    }
+}
